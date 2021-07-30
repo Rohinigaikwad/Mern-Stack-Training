b890ca4c3115ace27b51ec6d09149ce827e3faa6
Rohinigaikwad <rohinigaikwad66@.com>
2021-07-30 15:12:33 +0530

    commit for sorting

--- PracticeJSFiddle/array-operations.js
+++ PracticeJSFiddle/array-operations.js
@@ -57,6 +57,76 @@ console.log(vegetables);
 
 //Copy an Array
 let shallowCopy = fruits.slice();
-console.log(shallowCopy);
+console.log('Copy', shallowCopy);
 console.log(fruits);
 
+//sorting array
+let sort = fruits.sort();
+console.log('Sorting', sort);
+console.log(fruits)
+
+//reversing an array
+let sort1 = fruits.sort() //first sort the array then reverse 
+let rev = fruits.reverse();// Then reverse the order of the elements
+console.log('Reversing', rev)
+
+
+// Numeric Sort
+// By default, the sort() function sorts values as strings.
+
+// This works well for strings ("Apple" comes before "Banana").
+
+// However, if numbers are sorted as strings, "25" is bigger than "100", because "2" is bigger than "1".
+
+// Because of this, the sort() method will produce incorrect result when sorting numbers.
+
+// You can fix this by providing a compare function:
+
+//sort in ascending order
+const points = [40, 100, 1, 5, 25, 10];
+points.sort(function (a, b) {
+    return a - b
+});
+console.log('acending', points)
+
+
+//sort in descending order
+points.sort(function (a, b) {
+    return b - a;
+});
+console.log('descending', points)
+
+//sorting an array in ascending order
+let numbers = [1, 3, 4, 5, 6, 2];
+console.log(numbers.indexOf(2)); //index of wrongly placed element
+let temp = 0;
+for (let i = 0; i < numbers.length; i++)
+{
+    for (let j = i + 1; j < numbers.length; j++)
+    {
+        if (numbers[i] > numbers[j])
+        {
+            temp = numbers[i];
+            numbers[i] = numbers[j];
+            numbers[j] = temp;
+        }
+    }
+
+}
+console.log('Sorted in Ascending', numbers)
+
+
+//check array is sorted or not
+let array = [1, 3, 4, 5, 6, 2];
+function isSorted(array) {
+    const limit = array.length - 1;
+    for (let i = 0; i < limit; i++)
+    {
+        const current = array[i], next = array[i + 1];
+        if (current > next)
+        {
+            return false;
+        }
+    }
+    return true;
+}
